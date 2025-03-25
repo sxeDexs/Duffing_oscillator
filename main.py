229547d891c561a2ee2e35521cc7e4c9919624e7
Mark Markov <144445462+sxeDexs@users.noreply.github.com>
2025-03-25 17:16:31 +0300

    Correction

--- main.py
+++ main.py
@@ -5,60 +5,74 @@ import matplotlib.animation as animation
 
 # Загружаем данные
 data_rk4 = np.loadtxt("duffing_rk4.dat")
-data_dp = np.loadtxt("duffing_dp.dat")
-data_diff = np.loadtxt("diff.dat")  # Теперь содержит avg_local_diff_x и avg_local_diff_v
+data_dp8 = np.loadtxt("duffing_dp8.dat")
+data_rk6 = np.loadtxt("duffing_rk6.dat")
+data_diff_rk4_dp8 = np.loadtxt("diff_rk4_dp8.dat")  # Локальные разности RK4 vs Dopri8
+data_diff_rk6_dp8 = np.loadtxt("diff_rk6_dp8.dat")  # Локальные разности RK6 vs Dopri8
 
-N = data_rk4.shape[1] // 2  # Число точек
+N = data_rk4.shape[1] // 2  # Число частиц (количество пар x, v)
 steps = data_rk4.shape[0]   # Количество временных шагов
 
-x_rk4 = data_rk4[:, 0::2]
-v_rk4 = data_rk4[:, 1::2]
-x_dp = data_dp[:, 0::2]
-v_dp = data_dp[:, 1::2]
-avg_diff_x = data_diff[:, 0]  # Средняя локальная ошибка по x
-avg_diff_v = data_diff[:, 1]  # Средняя локальная ошибка по v
+# Извлекаем координаты и скорости
+x_rk4 = data_rk4[:, 0::2]  # Координаты x для RK4
+v_rk4 = data_rk4[:, 1::2]  # Скорости v для RK4
+x_dp8 = data_dp8[:, 0::2]  # Координаты x для Dopri8
+v_dp8 = data_dp8[:, 1::2]  # Скорости v для Dopri8
+x_rk6 = data_rk6[:, 0::2]  # Координаты x для RK6
+v_rk6 = data_rk6[:, 1::2]  # Скорости v для RK6
 
-time = np.linspace(0, 200.0, steps)
+# Извлекаем средние локальные разности
+avg_diff_x_rk4_dp8 = data_diff_rk4_dp8[:, 0]  # Средняя локальная ошибка по x (RK4 vs Dopri8)
+avg_diff_v_rk4_dp8 = data_diff_rk4_dp8[:, 1]  # Средняя локальная ошибка по v (RK4 vs Dopri8)
+avg_diff_x_rk6_dp8 = data_diff_rk6_dp8[:, 0]  # Средняя локальная ошибка по x (RK6 vs Dopri8)
+avg_diff_v_rk6_dp8 = data_diff_rk6_dp8[:, 1]  # Средняя локальная ошибка по v (RK6 vs Dopri8)
+
+time = np.linspace(0, 200.0, steps)  # Временная шкала
 
 # Создаем фигуру
 fig = plt.figure(figsize=(18, 12))
-plt.subplots_adjust(bottom=0.2)
+plt.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.2, hspace=0.3)
 
 # Графики фазового пространства
-ax1 = plt.subplot(2, 2, 1)
-ax2 = plt.subplot(2, 2, 2)
+ax1 = plt.subplot(2, 3, 1)  # RK4
+ax2 = plt.subplot(2, 3, 2)  # Dopri8
+ax3 = plt.subplot(2, 3, 3)  # RK6
 # Графики разностей
-ax4 = plt.subplot(2, 2, 3)
-ax5 = plt.subplot(2, 2, 4)
+ax4 = plt.subplot(2, 3, 4)  # Ошибки RK4 vs Dopri8
+ax5 = plt.subplot(2, 3, 5)  # Ошибки RK6 vs Dopri8
 
 # Настройка фазовых графиков
-for ax in (ax1, ax2):
-    ax.set_xlim(np.min([x_rk4, x_dp]) - 0.5, np.max([x_rk4, x_dp]) + 0.5)
-    ax.set_ylim(np.min([v_rk4, v_dp]) - 0.5, np.max([v_rk4, v_dp]) + 0.5)
+for ax in (ax1, ax2, ax3):
+    ax.set_xlim(np.min([x_rk4, x_dp8, x_rk6]) - 0.5, np.max([x_rk4, x_dp8, x_rk6]) + 0.5)
+    ax.set_ylim(np.min([v_rk4, v_dp8, v_rk6]) - 0.5, np.max([v_rk4, v_dp8, v_rk6]) + 0.5)
     ax.set_xlabel("x")
     ax.set_ylabel("v")
 
 ax1.set_title("RK4")
-ax2.set_title("Dormand-Prince 8(5,3)")
+ax2.set_title("Dopri8")
+ax3.set_title("RK6")
 
 # Настройка графиков ошибок
 ax4.set_xlabel("Time")
-ax4.set_ylabel("Average |x_RK4 - x_DP8|")
-ax4.set_title("Average Local Error in x")
+ax4.set_ylabel("Average |x - x_Dopri8|")
+ax4.set_title("Average Local Error in x (RK4 vs Dopri8)")
 ax4.set_xlim(0, 200.0)
-ax4.set_ylim(0, 3.5)  # Устанавливаем максимальное значение для оси Y
+ax4.set_ylim(0, np.max([avg_diff_x_rk4_dp8, avg_diff_v_rk4_dp8]) * 1.1)  # Динамическая шкала
 
 ax5.set_xlabel("Time")
-ax5.set_ylabel("Average |v_RK4 - v_DP8|")
-ax5.set_title("Average Local Error in v")
+ax5.set_ylabel("Average |x - x_Dopri8|")
+ax5.set_title("Average Local Error in x (RK6 vs Dopri8)")
 ax5.set_xlim(0, 200.0)
-ax5.set_ylim(0, 3.5)  # Устанавливаем максимальное значение для оси Y
+ax5.set_ylim(0, np.max([avg_diff_x_rk6_dp8, avg_diff_v_rk6_dp8]) * 1.1)  # Динамическая шкала
 
 # Графические объекты
 points_rk4 = ax1.scatter(x_rk4[0], v_rk4[0], c=np.linspace(0, 1, N), cmap='plasma', s=10)
-points_dp = ax2.scatter(x_dp[0], v_dp[0], c=np.linspace(0, 1, N), cmap='plasma', s=10)
-line_x, = ax4.plot([], [], 'b-', label='Error in x')
-line_v, = ax5.plot([], [], 'r-', label='Error in v')
+points_dp8 = ax2.scatter(x_dp8[0], v_dp8[0], c=np.linspace(0, 1, N), cmap='plasma', s=10)
+points_rk6 = ax3.scatter(x_rk6[0], v_rk6[0], c=np.linspace(0, 1, N), cmap='plasma', s=10)
+line_x_rk4_dp8, = ax4.plot([], [], 'b-', label='Error in x (RK4)')
+line_v_rk4_dp8, = ax4.plot([], [], 'r-', label='Error in v (RK4)')
+line_x_rk6_dp8, = ax5.plot([], [], 'b-', label='Error in x (RK6)')
+line_v_rk6_dp8, = ax5.plot([], [], 'r-', label='Error in v (RK6)')
 
 ax4.legend()
 ax5.legend()
@@ -78,13 +92,19 @@ current_frame = 0
 # Функция обновления графиков
 def update(frame):
     points_rk4.set_offsets(np.column_stack((x_rk4[frame], v_rk4[frame])))
-    points_dp.set_offsets(np.column_stack((x_dp[frame], v_dp[frame])))
-    line_x.set_data(time[:frame + 1], avg_diff_x[:frame + 1])
-    line_v.set_data(time[:frame + 1], avg_diff_v[:frame + 1])
+    points_dp8.set_offsets(np.column_stack((x_dp8[frame], v_dp8[frame])))
+    points_rk6.set_offsets(np.column_stack((x_rk6[frame], v_rk6[frame])))
+    line_x_rk4_dp8.set_data(time[:frame + 1], avg_diff_x_rk4_dp8[:frame + 1])
+    line_v_rk4_dp8.set_data(time[:frame + 1], avg_diff_v_rk4_dp8[:frame + 1])
+    line_x_rk6_dp8.set_data(time[:frame + 1], avg_diff_x_rk6_dp8[:frame + 1])
+    line_v_rk6_dp8.set_data(time[:frame + 1], avg_diff_v_rk6_dp8[:frame + 1])
     points_rk4.set_visible(True)
-    points_dp.set_visible(True)
-    line_x.set_visible(True)
-    line_v.set_visible(True)
+    points_dp8.set_visible(True)
+    points_rk6.set_visible(True)
+    line_x_rk4_dp8.set_visible(True)
+    line_v_rk4_dp8.set_visible(True)
+    line_x_rk6_dp8.set_visible(True)
+    line_v_rk6_dp8.set_visible(True)
     fig.canvas.draw_idle()
 
 # Функция для слайдера
@@ -98,18 +118,21 @@ def animate(frame):
     global current_frame
     current_frame = frame
     points_rk4.set_offsets(np.column_stack((x_rk4[frame], v_rk4[frame])))
-    points_dp.set_offsets(np.column_stack((x_dp[frame], v_dp[frame])))
-    line_x.set_data(time[:frame + 1], avg_diff_x[:frame + 1])
-    line_v.set_data(time[:frame + 1], avg_diff_v[:frame + 1])
+    points_dp8.set_offsets(np.column_stack((x_dp8[frame], v_dp8[frame])))
+    points_rk6.set_offsets(np.column_stack((x_rk6[frame], v_rk6[frame])))
+    line_x_rk4_dp8.set_data(time[:frame + 1], avg_diff_x_rk4_dp8[:frame + 1])
+    line_v_rk4_dp8.set_data(time[:frame + 1], avg_diff_v_rk4_dp8[:frame + 1])
+    line_x_rk6_dp8.set_data(time[:frame + 1], avg_diff_x_rk6_dp8[:frame + 1])
+    line_v_rk6_dp8.set_data(time[:frame + 1], avg_diff_v_rk6_dp8[:frame + 1])
     slider.set_val(frame)
-    return points_rk4, points_dp, line_x, line_v
+    return points_rk4, points_dp8, points_rk6, line_x_rk4_dp8, line_v_rk4_dp8, line_x_rk6_dp8, line_v_rk6_dp8
 
 # Функция запуска анимации
 def play(event):
     global anim
     if anim is None or anim.event_source is None:
         anim = animation.FuncAnimation(
-            fig, animate, frames=range(current_frame, steps), interval=5, blit=False, repeat=True
+            fig, animate, frames=range(current_frame, steps), interval=1, blit=False, repeat=True
         )
     fig.canvas.draw_idle()
 
